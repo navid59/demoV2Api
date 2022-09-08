e5473c69e7b0d47a5b18407c3db1e4aed5f3ab4a
Navid 59 <torabazary@yahoo.com>
2022-09-08 10:52:29 +0300

    Just Update

--- example/classes/log.php
+++ example/classes/log.php
@@ -76,4 +76,47 @@ class log {
         ob_end_clean();
            file_put_contents('logs/ipnLog.log', $contents , FILE_APPEND | LOCK_EX);
     }
+
+    static function logHeader() {
+        $targetFile = 'logs/header.log';
+		$data = sprintf(
+			"%s %s %s\n\nHTTP headers:\n",
+			$_SERVER['REQUEST_METHOD'],
+			$_SERVER['REQUEST_URI'],
+			$_SERVER['SERVER_PROTOCOL']
+		);
+
+		foreach (SELF::getHeaderList() as $name => $value) {
+			$data .= $name . ': ' . $value . "\n";
+		}
+
+		$data .= "\nRequest body:\n";
+        
+		file_put_contents(
+			$targetFile,
+			$data . file_get_contents('php://input') . "\n", FILE_APPEND
+		);
+		
+		file_put_contents(
+			$targetFile,
+			$data . print_r($_REQUEST,true) . "\n*************************************************************\n", FILE_APPEND
+		);
+	}
+
+    static function getHeaderList() {
+		$headerList = [];
+		foreach ($_SERVER as $name => $value) {
+			if (preg_match('/^HTTP_/',$name)) {
+				// convert HTTP_HEADER_NAME to Header-Name
+				$name = strtr(substr($name,5),'_',' ');
+				$name = ucwords(strtolower($name));
+				$name = strtr($name,' ','-');
+
+				// add to list
+				$headerList[$name] = $value;
+			}
+		}
+
+		return $headerList;
+	}
 }
--- example/ipn.php
+++ example/ipn.php
@@ -12,40 +12,22 @@ require_once 'vendor/autoload.php';
 // Log
 $setRealTimeLog = ["IPN"    =>  "IPN Is hitting"];
 log::setRealTimeLog($setRealTimeLog);
+log::logHeader();
 
 /**
  * get defined keys
  */
 $ntpIpn = new IPN();
 
-$ntpIpn->activeKey         = 'XXXX-XXXX-XXXX-XXXX-XXXX'; // activeKey or posSignature
-$ntpIpn->posSignatureSet[] = 'XXXX-XXXX-XXXX-XXXX-XXXX';
-$ntpIpn->posSignatureSet[] = 'XXXX-ZZZZ-WWWW-KKKK-NNNN_fake1'; 
-$ntpIpn->posSignatureSet[] = 'ZZZZ-XXXX-ZZZZ-GC8B-NNNN_fake2'; 
-$ntpIpn->posSignatureSet[] = 'KKKK-3WDM-XXXX-KKKK-NNNN_fake3';
+$ntpIpn->activeKey         = 'AAAA-BBBB-CCCC-DDDD-EEEE'; // activeKey or posSignature
+$ntpIpn->posSignatureSet[] = 'AAAA-BBBB-CCCC-DDDD-FFFF'; // The active key should be in posSignatureSet as well
+$ntpIpn->posSignatureSet[] = 'AAAA-BBBB-CCCC-DDDD-GGGG'; 
+$ntpIpn->posSignatureSet[] = 'AAAA-BBBB-CCCC-DDDD-HHHH'; 
+$ntpIpn->posSignatureSet[] = 'AAAA-BBBB-CCCC-DDDD-IIII';
 $ntpIpn->hashMethod        = 'SHA512';
 $ntpIpn->alg               = 'RS512';
-// $ntpIpn->publicKeyStr      = '-----BEGIN CERTIFICATE-----
-// MIIDKLOREMNgAwIBAgIBADANBgkqhkiG9w0BAQQFADCBsTELMAkGA1UEBhMCUk8x
-// EjAQBLOREMNLOREMN2hhcmVzdDESMBAGA1UEBwwJQnVjaGFyZXN0MRcwFQYDVQQK
-// DA5OIEUgVCBLOREMNOREMNEnMCUGA1UECwweTiBFIFQgTyBQIEkgQSBEZXZlbG9w
-// bWVudLOREMNtMRQwLOREMNOREMNtb2JpbHBheS5ybzEiMCAGCSqGSIb3DQEJARYT
-// c3VwcG9ydEBLOREMNHBheLOREMNEMN0yMTA0MjcxMTMxMDdaFw0yMjA0MjcxMTMx
-// MDdLOREMNQswCQYDLOREMNJSLOREMNREMNUECAwJQnVjaGFyZXN0MRIwEAYDVQQH
-// DAlCdWNoYXJlc3QxFzAVBLOREMNMLOREMNREMN8gUCBJIEEgMScwJQYDVQQLDB5O
-// IEUgVCBLOREMNSBBIERldmVsLOREMN50LOREMNREMNASBgNVBAMMC21vYmlscGF5
-// LnJvMSIwIAYJKLOREMNNAQkBFhNzLOREMNJ0LOREMNOREMN5LnJvMIGfMA0GCSqG
-// SIb3DQEBAQUAA4GNADCBiQKBgQC8IdPzLOREMNir4LOREMNOREMNOTFjQoeNtpHH
-// xSm6j+WFYglAYNzHOWWHdXLOREMNtUCNmf47LOREMNRkMILOREMN0vW6MBxJGR/N
-// WaJTqDxwWW2KQNvALOREMNGk14y7YgRr46cLs5Y5lLOREMNpyGhNLOREMN/TC1ht
-// nxjHXQIDAQABLOREMNAdBgNVHQ4EFgQUPclwoTBsc1M0H5LOREMNLOREMNLOREMN
-// VR0jBBgwFoAUPclwoTBsc1M0H5ZpF09aMiAaHrUwDAYDVR0TBAUwAwEB/zLOREMN
-// hkiG9wLOREMNAAOBgQB5juqDH6s09OmEcRmfbspXGVyxIaFMMgAOLOREMNezVKOv
-// LOREMNvO8ZIUy9G/P87qNz9WIe5ryfAR9G/ZkA0u8dTWxBElkvJT01q4ejLOREMN
-// wvLWzfOJcGTsfYy0MnMHGiq/0JJ11foTA6ZofudJhJ8UjXQ7waKDOFbnqKPGFQ==
-// -----END CERTIFICATE-----';
 
-
+$ntpIpn->publicKeyStr = "-----BEGIN PUBLIC KEY-----\nYourPublicKeyIsHere-----END PUBLIC KEY-----\n";
 
 $ipnResponse = $ntpIpn->verifyIPN();
 
